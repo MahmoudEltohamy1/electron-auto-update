ee2fb6bb42502c9e87e7b7192fc671f6b95801c5
mahmoud.eltohamy <mahmoud.eltohamy@qeema.net>
2025-06-16 17:22:45 +0300

    edit update behaviour

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-const { app, BrowserWindow, autoUpdater, dialog } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const { autoUpdater } = require('electron-updater');
 const path = require('path');
+
+// Configure the update server
+autoUpdater.setFeedURL({
+  provider: 'github',
+  owner: 'MahmoudEltohamy1',
+  repo: 'electron-auto-update',
+});
+
+// Optional: Configure auto-updater settings
+autoUpdater.autoDownload = false; // Don't auto-download updates
+autoUpdater.autoInstallOnAppQuit = true;
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -7,31 +19,91 @@ function createWindow() {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
     },
   });
 
-  win.loadFile('index.html');
+  win.loadFile('./index.html');
+  
+  // Open DevTools in development
+  if (process.env.NODE_ENV === 'development') {
+    win.webContents.openDevTools();
+  }
 }
 
 app.whenReady().then(() => {
   createWindow();
 
-  autoUpdater.checkForUpdatesAndNotify();
+  // Check for updates after app is ready
+  checkForUpdates();
 });
 
-autoUpdater.on('update-downloaded', () => {
-  dialog
-    .showMessageBox({
-      type: 'info',
-      title: 'Update Available',
-      message: 'Update downloaded. It will be installed on restart.',
-      buttons: ['Restart Now', 'Later'],
-    })
-    .then((result) => {
-      if (result.response === 0) autoUpdater.quitAndInstall();
-    });
+function checkForUpdates() {
+  autoUpdater.checkForUpdatesAndNotify();
+}
+
+// Auto-updater events
+autoUpdater.on('checking-for-update', () => {
+  console.log('Checking for update...');
+});
+
+autoUpdater.on('update-available', (info) => {
+  console.log('Update available:', info.version);
+  
+  dialog.showMessageBox({
+    type: 'info',
+    title: 'Update Available',
+    message: `A new version (${info.version}) is available. Do you want to download it now?`,
+    buttons: ['Download', 'Later'],
+    defaultId: 0
+  }).then((result) => {
+    if (result.response === 0) {
+      autoUpdater.downloadUpdate();
+    }
+  });
+});
+
+autoUpdater.on('update-not-available', (info) => {
+  console.log('Update not available:', info.version);
+});
+
+autoUpdater.on('error', (err) => {
+  console.error('Error in auto-updater:', err);
+  dialog.showErrorBox('Update Error', `Error checking for updates: ${err.message}`);
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  console.log(log_message);
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+  console.log('Update downloaded:', info.version);
+  
+  dialog.showMessageBox({
+    type: 'info',
+    title: 'Update Ready',
+    message: `Update (${info.version}) downloaded. The application will restart to apply the update.`,
+    buttons: ['Restart Now', 'Later'],
+    defaultId: 0
+  }).then((result) => {
+    if (result.response === 0) {
+      autoUpdater.quitAndInstall();
+    }
+  });
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
